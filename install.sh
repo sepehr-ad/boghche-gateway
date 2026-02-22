b5f19da9dcbaddc4619ff7c2b921a04bb4b5e6f6
Sepehr Ad <sepehr.adibi.sa@gmail.com>
2026-02-22 13:41:56 +0000

    Fix install.sh full boghche rebrand

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 
 REPO="https://raw.githubusercontent.com/sepehr-ad/boghche-gateway/main"
 
-echo "[+] Installing Baghcheh Gateway..."
+echo "[+] Installing Boghche Gateway..."
 
 apt update
 apt install -y jq curl
-
-REPO="https://raw.githubusercontent.com/sepehr-ad/boghche-gateway/main"
 
 mkdir -p /usr/local/lib/boghche
 mkdir -p /etc/boghche
@@ -15,11 +13,12 @@ mkdir -p /etc/boghche
 curl -sL $REPO/lib/vti-engine.sh -o /usr/local/lib/boghche/vti-engine.sh
 curl -sL $REPO/bin/boghche -o /usr/local/bin/boghche
 curl -sL $REPO/systemd/boghche.service -o /etc/systemd/system/boghche.service
-chmod +x /usr/local/lib/baghcheh/vti-engine.sh
-chmod +x /usr/local/bin/baghcheh
+
+chmod +x /usr/local/lib/boghche/vti-engine.sh
+chmod +x /usr/local/bin/boghche
 
 systemctl daemon-reload
-systemctl enable baghcheh
+systemctl enable boghche
 
 echo "[✓] Installation Complete."
-echo "Run: sudo baghcheh init"
+echo "Run: sudo boghche init"
