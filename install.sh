64ba937a78a25793862ca920455dac24a083a580
sepehr-ad <sepehradibi1375@gmail.com>
2026-05-12 14:28:30 +0330

    Persist rp_filter=0 defaults for VTI route mode

--- install.sh
+++ install.sh
@@ -69,6 +69,8 @@ configure_kernel() {
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.rp_filter=0
 net.ipv4.conf.default.rp_filter=0
+net.ipv4.conf.eth0.rp_filter=0
+net.ipv4.conf.vti0.rp_filter=0
 net.ipv4.conf.all.accept_redirects=0
 net.ipv4.conf.default.accept_redirects=0
 EOF
